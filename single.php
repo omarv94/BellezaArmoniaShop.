ed6853cabcc65d129d6392dd2a5d5b005529cade
Omar Valencia <omarvalencia9415@gmail.com>
2020-09-15 16:45:52 -0500

    create posttype productos

---
+++ single.php
@@ -0,0 +1,21 @@
+<?get_header();?>
+<main class=container>
+<?php if(have_posts()){
+    while(have_posts()){
+        the_post();
+        ?>
+        <h1 class="my-3"><?php the_title()?></h1>
+        <div class="row">
+            <div class="col-6">
+                <?php the_post_thumbnail('medium');?>
+            </div>
+            <div class="col-6">
+                <?php the_content();?>
+            </div>
+        </div>
+        <?php
+    }
+}?>
+</main>
+
+<?get_footer();?>
